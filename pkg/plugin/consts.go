e78ad8477b3525fefdab9d84546b010de3289435
maor-mil <maor2475@gmail.com>
2023-12-26 13:51:50 +0200

    adding consts

---
+++ pkg/plugin/consts.go
@@ -0,0 +1,5 @@
+package plugin
+
+const STREAM_PATH = "rabbitmq"
+const FRAME_NAME = "rabbitmq"
+const TIMESTAMP_NAME = "RmqMsgConsumedTimestamp"
--- pkg/plugin/query.go
+++ pkg/plugin/query.go
@@ -11,7 +11,7 @@ import (
 )
 
 func (ds *RabbitMQDatasource) QueryData(_ context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	log.DefaultLogger.Debug("Started QueryData method!")
+	log.DefaultLogger.Info("Started QueryData method!")
 	response := backend.NewQueryDataResponse()
 
 	for _, q := range req.Queries {
@@ -20,13 +20,13 @@ func (ds *RabbitMQDatasource) QueryData(_ context.Context, req *backend.QueryDat
 		response.Responses[q.RefID] = res
 	}
 
-	log.DefaultLogger.Debug("Finished QueryData method!")
+	log.DefaultLogger.Info("Finished QueryData method!")
 
 	return response, nil
 }
 
 func (ds *RabbitMQDatasource) query(pCtx backend.PluginContext, query backend.DataQuery) backend.DataResponse {
-	log.DefaultLogger.Debug("Started query method!")
+	log.DefaultLogger.Info("Started query method!")
 	response := backend.DataResponse{}
 	var qm QueryModel
 	response.Error = json.Unmarshal(query.JSON, &qm)
@@ -35,18 +35,18 @@ func (ds *RabbitMQDatasource) query(pCtx backend.PluginContext, query backend.Da
 		return response
 	}
 
-	frame := data.NewFrame("rabbitmq")
+	frame := data.NewFrame(FRAME_NAME)
 
 	channel := live.Channel{
 		Scope:     live.ScopeDatasource,
 		Namespace: pCtx.DataSourceInstanceSettings.UID,
-		Path:      "rabbitmq",
+		Path:      STREAM_PATH,
 	}
 	frame.SetMeta(&data.FrameMeta{Channel: channel.String()})
 
 	response.Frames = append(response.Frames, frame)
 
-	log.DefaultLogger.Debug("Finished query method!")
+	log.DefaultLogger.Info("Finished query method!")
 
 	return response
 }
